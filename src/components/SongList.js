aef3d4ff9259c53eb31d40b079a87725a5e9cb27
Palmi Moller <palmimoller@me.com>
2020-06-22 10:51:14 -0600

    Wired up Redux Store to SongList component with connect and mapStateToProps

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -3,8 +3,15 @@ import { connect } from 'react-redux';
 
 class SongList extends Component {
   render() {
+    console.log('this.props', this.props);
     return <div>Song List</div>;
   }
 }
 
-export default connect()(SongList);
+const mapStateToProps = state => {
+  console.log('state', state);
+
+  return { songs: state.songs };
+};
+
+export default connect(mapStateToProps)(SongList);
