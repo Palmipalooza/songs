6a3a9c980485c59f9223bc378ad75cf2714076b3
Palmi Moller <palmimoller@me.com>
2020-06-26 15:55:10 -0600

    SongDetail bug fix connect function, App renders SongDetail component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-// import { selectSong } from '../actions';
 import SongList from './SongList';
+import SongDetail from './SongDetail';
 
 const App = () => {
   return (
     <div className="ui container grid">
       <div className="ui row">
         <div className="column eight wide">
+          <div className="column eight wide">
+            <SongDetail />
+          </div>
           <SongList />
         </div>
       </div>
--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import connect from 'react-redux';
+import { connect } from 'react-redux';
 
-const SongDetail = () => {
+const SongDetail = props => {
+  console.log('props', props);
   return <div>Song Detail</div>;
 };
 
