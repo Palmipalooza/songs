5a7444cd7abedc3ada8841f127fe8d64a82a26d1
Palmi Moller <palmimoller@me.com>
2020-06-26 15:59:46 -0600

    App - styling fix

--- src/components/App.js
+++ src/components/App.js
@@ -7,9 +7,9 @@ const App = () => {
     <div className="ui container grid">
       <div className="ui row">
         <div className="column eight wide">
-          <div className="column eight wide">
-            <SongDetail />
-          </div>
+          <SongDetail />
+        </div>
+        <div className="column eight wide">
           <SongList />
         </div>
       </div>
